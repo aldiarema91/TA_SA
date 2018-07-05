fea1d1304f29185acc5c2373bb5cb7dcc2f6e209
aldiarema91 <39393012+aldiarema91@users.noreply.github.com>
2018-07-05 12:33:49 +0700

    Progress 1
    membuat map & cek keberadaan Queen

--- test.java
+++ test.java
@@ -1,27 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package n.queen;
+package nqueens;
 
-/**
- *
- * @author Aldi
- */
-public class test {
+import java.util.Scanner;
+
+public class NQueens
+{
     
-	void printMap(int Map[][], int N)
-    {
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < N; j++)
-                System.out.print(" " + Map[i][j]
-                                 + " ");
-            System.out.println();
-        }
-    }
-
+    
     public static void main(String args[])
     {
         int sd=0;
@@ -30,11 +14,42 @@ public class test {
             System.out.print("Map = ");
             sd = in.nextInt();
             if (sd>3) {
-            	int Map[][] = new int[sd][sd];
-                printMap(Map, sd);
+                int Map[][] = new int[sd][sd];
+                Map[0][0] = 1;
+                
+                
+                for (int i = 0; i < sd; i++){
+                    for (int j = 0; j < sd; j++){
+                        //cek kesamping
+                        for (int k = 0; k < j; k++) {
+                            if (Map[k][i]==1) {
+                                System.out.print("a");
+                            }
+                        }
+                        //cek kolom
+                        for (int k = 0; k < i; k++) {
+                            if (Map[k][j]==1) {
+                                System.out.print("a");
+                            }
+                        }
+                        //cek miring
+                        for (int a=i, b=j; a>=0 && b>=0; a--, b--){
+                            if (Map[a][b]==1) {
+                                System.out.print("a");
+                            }
+                        }
+                        System.out.print(" " + Map[i][j]+ " ");
+                    }
+                    System.out.println();
+                }
                 break;
             }
             System.out.println("Masukan lebih dari 3");
         }
     }
+    void printMap(int N, int mape[][])
+    {
+        
+    }
 }
+

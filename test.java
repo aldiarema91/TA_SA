5c00875dca8d88f4262bbc2a258cf6ab73101363
aldiarema91 <39393012+aldiarema91@users.noreply.github.com>
2018-07-05 12:41:10 +0700

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ package nqueens;
 
 import java.util.Scanner;
 
+
 public class NQueens
 {
     
@@ -49,7 +50,12 @@ public class NQueens
     }
     void printMap(int N, int mape[][])
     {
-        
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
     }
 }
 
